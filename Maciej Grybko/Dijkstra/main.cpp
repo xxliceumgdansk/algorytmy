06884cb127ea9d094776df9c0a3f187ae1277843
Kacper <Cielak1357@gmail.com>
2015-06-17 23:34:06 +0200

    Fixed accidental commit

--- Maciej Grybko/Dijkstra/main.cpp
+++ Maciej Grybko/Dijkstra/main.cpp
@@ -1,94 +1,80 @@
 #include<iostream>
 
 using namespace std;
+
 int main()
 {
-    //MOJE init
-    int vertexes, edges;
-    int beginningVertex, endingVertex;
+int vertexes, edges;
+cin >> vertexes >> edges;
+int beginningOfEdge[edges];
+int endOfEdge[edges];
+int CostOfEdge[edges+1];//ostatnia komorka, to
+int beginningVertex, endingVertex;
 
-    cin >> vertexes >> edges;
+for(int i=0; i<edges; i++)
+{
+    cin >> beginningOfEdge[i] >> endOfEdge[i] >> CostOfEdge[i];
+}
+cin >> beginningVertex >> endingVertex;
 
-    //MOJE edge
-    int beginningOfEdge[edges];
-    int endOfEdge[edges];
-    int CostOfEdge[edges+1];//ostatnia komorka, to //MOJE TODO czemu z duzej litery?!!!!!!!!!
-    //edge
+int minCost[vertexes];
+int predecessor[vertexes];
+bool checkedVertexes[vertexes];
+int actualVertex = beginningVertex;
+//int actualCosts[];
+int adjacentVertexes[edges][vertexes];
+//int neighbourNumber=0;
 
+for(int j=0; j<vertexes; j++)
+{
     for(int i=0; i<edges; i++)
     {
-        cin >> beginningOfEdge[i] >> endOfEdge[i] >> CostOfEdge[i];
+        adjacentVertexes[i][j]=-1; //przypisanie liczby ujemnej kazdemu sasiadowi kazdego wierzcholka sluzy odroznieniu sasiada, ktory istnieje, od tego, ktory nie istnieje
     }
-    cin >> beginningVertex >> endingVertex;
+}
 
-    //MOJE vertex
-    int minCost[vertexes];
-    int predecessor[vertexes];
-    bool checkedVertexes[vertexes];
-    //vertex
+for(int i=0; i<vertexes; i++)
+{
+    minCost[i]=2147483647;
+    predecessor[i]=-1;
+    checkedVertexes[i]=false;
+}
 
-    int actualVertex = beginningVertex-1;
-    int adjacentVertexes[edges][vertexes]; //neighbours
+CostOfEdge[edges+1]=2147483647;
 
-    for(int j=0; j<vertexes; j++)
+
+for(int j=0; j<vertexes; j++)//znajdowanie sasiadow kazdego wierzcholka
+{
+    for(int i=0; i<edges; i++)
     {
-        for(int i=0; i<edges; i++)
+        if(beginningOfEdge[i]==j)adjacentVertexes[i][j]=endOfEdge[i]; //jezeli poczatek krawedzi i jest wierzcholkiem j, to jego sasiadem jest koniec krawedzi i
+    }
+}
+for(int j=0; j<vertexes; j++)
+{
+    for(int i=0; i<edges; i++)
+    {
+        if(checkedVertexes[endOfEdge[i]]==false&&CostOfEdge[i]<CostOfEdge[actualVertex])
         {
-            adjacentVertexes[i][j]=-1; //przypisanie liczby ujemnej kazdemu sasiadowi kazdego wierzcholka sluzy odroznieniu sasiada, ktory istnieje, od tego, ktory nie istnieje
+            actualVertex=endOfEdge[i];
         }
     }
-
-    for(int i=0; i<vertexes; i++)
+    for(int i=0; i<edges; i++)
     {
-        minCost[i]=2147483647; //MOJE infinity
-        predecessor[i]=-1;
-        checkedVertexes[i]=false;
-    }
 
-    CostOfEdge[edges+1]=2147483647; //MOJE TODO chyba wywalic bo ma oznaczac koszt od poczatku do konca
-
-    //finding neighbours
-    for(int j=0; j<vertexes; j++)//znajdowanie sasiadow kazdego wierzcholka //MOJE TODO powinno byc na odwrot bo czytelniej jest szukac wierzcholka dla krawedzi, nie na odwrot
-    {
-        for(int i=0; i<edges; i++)
+        if(minCost[adjacentVertexes[i][actualVertex]] > minCost[actualVertex] + CostOfEdge[actualVertex])
         {
-            if(beginningOfEdge[i]==j)
-                adjacentVertexes[i][j]=endOfEdge[i]-1; //jezeli poczatek krawedzi i jest wierzcholkiem j, to jego sasiadem jest koniec krawedzi i
+            minCost[actualVertex] = minCost[j] + CostOfEdge[i];
+            predecessor[adjacentVertexes[i][actualVertex]] = actualVertex;
         }
+        actualVertex=edges+1;//sluzy do tego, zebysmy przez przypadek nie nadali ponownie (innej) wartosci minCost - nie wiem, czy to w ogole jest potrzebne
     }
-    //finding neighbours
-    //init
+    checkedVertexes[endOfEdge[actualVertex]]=true;
+}
 
-    cout << "zainicjalizowano!\n";
-    for(int j=0; j<vertexes; j++)
-    {
-        for(int i=0; i<edges; i++)
-        {
-            if(checkedVertexes[endOfEdge[i]-1]==false && CostOfEdge[i]<CostOfEdge[actualVertex]) //MOJE odjalem 1 od endOfEdge[i] bo jest on w cin podane jako liczba liczac od 1, a tablica checkedVertexes jest od 0
-            {
-                actualVertex=endOfEdge[i]-1;
-            }
-        }
+for(int i=0; i<edges; i++) cout <<  minCost[i];
+cout << endl;
+for(int i=0; i<edges; i++) cout <<  predecessor[i];
 
-        for(int i=0; i<edges; i++)
-        {
-
-            if(minCost[adjacentVertexes[i][actualVertex]] > minCost[actualVertex] + CostOfEdge[actualVertex])
-            {
-                minCost[actualVertex] = minCost[j] + CostOfEdge[i];
-                predecessor[adjacentVertexes[i][actualVertex]] = actualVertex;
-                break;
-            }
-            //actualVertex=edges+1;//sluzy do tego, zebysmy przez przypadek nie nadali ponownie (innej) wartosci minCost - nie wiem, czy to w ogole jest potrzebne
-        }
-        checkedVertexes[endOfEdge[actualVertex]]=true; // odjalem 1 (patrz linijka 67 czemu)
-    }
-
-    cout << "done!\n";
-    cout << minCost[endingVertex];
-    /*for(int i=0; i<edges; i++) cout <<  minCost[i];
-    cout << endl;
-    for(int i=0; i<edges; i++) cout <<  predecessor[i];*/
-
-    return 0;
+return 0;
 }
