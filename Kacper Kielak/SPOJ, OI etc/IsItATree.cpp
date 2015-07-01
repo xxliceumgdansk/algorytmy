0ee65192bc4981701e87f4cdfc7343cbbfed6e36
Kacper <Cielak1357@gmail.com>
2015-07-01 23:23:44 +0200

    added 'is it a tree' problem with debug output

---
+++ Kacper Kielak/SPOJ, OI etc/IsItATree.cpp
@@ -0,0 +1,88 @@
+//SPOJ problem: http://www.spoj.com/problems/PT07Y/s
+
+#include <cstdio>
+#include <vector>
+
+#define UNDEF -1
+
+struct node
+{
+    int parent;
+    std::vector<int> neighbours;
+    int checkedNeighbours;
+    bool visited;
+};
+
+node* generateGraph(int nodesNumber, int edgesNumber)
+{
+    node *nodes = new node[nodesNumber];
+    for(int i=0; i<edgesNumber; i++)
+    {
+        int start, finish;
+        scanf("%d", &start); scanf("%d", &finish);
+        start--; finish--;
+        nodes[start].neighbours.push_back(finish);
+        nodes[finish].neighbours.push_back(start);
+    }
+    for(int i=0; i<nodesNumber; i++)
+    {
+        nodes[i].checkedNeighbours=0;
+        nodes[i].parent=UNDEF;
+        nodes[i].visited=false;
+    }
+    printf("graph generated!\n");
+    return nodes;
+}
+
+int getNextNodeNumber(int currentNodeNumber, node* nodes)
+{
+    while(nodes[currentNodeNumber].checkedNeighbours==nodes[currentNodeNumber].neighbours.size())
+    {
+        if(nodes[currentNodeNumber].parent==UNDEF)
+            return UNDEF;
+
+        currentNodeNumber=nodes[currentNodeNumber].parent;
+    }
+
+    int nextNodeNumber = nodes[currentNodeNumber].neighbours[nodes[currentNodeNumber].checkedNeighbours];
+    nodes[currentNodeNumber].checkedNeighbours++;
+
+    if(nextNodeNumber==nodes[currentNodeNumber].parent)
+        nextNodeNumber=getNextNodeNumber(currentNodeNumber, nodes);
+    else
+        nodes[nextNodeNumber].parent=currentNodeNumber;
+
+    return nextNodeNumber;
+}
+
+int main()
+{
+    int nodesNumber, edgesNumber;
+    scanf("%d", &nodesNumber); scanf("%d", &edgesNumber);
+    printf("Nodes and edges numberes scaned!\n");
+    node *nodes = generateGraph(nodesNumber, edgesNumber);
+    int currentNodeNumber=0;
+
+    while(currentNodeNumber!=UNDEF)
+    {
+        printf("Current node number is: %d, ", currentNodeNumber);
+        if(nodes[currentNodeNumber].visited)
+        {
+            printf("NO");
+            return 0;
+        }
+        printf("and it wasn't visited yet!\n");
+        nodes[currentNodeNumber].visited=true;
+        currentNodeNumber=getNextNodeNumber(currentNodeNumber, nodes);
+    }
+
+    for(int i=0; i<nodesNumber; i++)
+        if(nodes[i].neighbours.empty())
+        {
+            printf("NO");
+            return 0;
+        }
+
+    printf("YES");
+    return 0;
+}
